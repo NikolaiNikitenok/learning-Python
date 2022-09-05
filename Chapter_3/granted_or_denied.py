79b76db4f5bf53c9fe672a1934cbaeadc579cbfb
Nikolai Nikitenok <104710310+NikolaiNikitenok@users.noreply.github.com>
2022-09-05 20:57:32 +0300

    .

---
+++ Chapter_3/granted_or_denied.py
@@ -0,0 +1,15 @@
+# Открыто/Закрыто
+
+# Демонстрирует использование условия else
+
+print("Welcome to System Security Inc.")
+print("-- where security is our middle name\n")
+
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+input("\n\nPress the enter key to exit.")
---
+++ Chapter_3/password.py
@@ -0,0 +1,13 @@
+# Пароль
+
+# Демонстрирует принцип конструкции if
+
+print("Welcome to System Security Inc.")
+print("-- where security is our middle name\n")
+
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("Access Granted")
+
+input("\n\nPress the enter key to exit.")
