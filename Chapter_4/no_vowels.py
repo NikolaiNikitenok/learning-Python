c4ad20e251e4c0f983a175363eb863ee2d10aa8e
Nikolai Nikitenok <104710310+NikolaiNikitenok@users.noreply.github.com>
2022-09-16 23:25:42 +0300

    .

--- Chapter_4/no_vowels.py
+++ Chapter_4/no_vowels.py
@@ -4,10 +4,10 @@
 
 message = input("Введите текст: ")
 new_message = ""
-VOMELS = "аеоиёоуыэяю"
+VOWELS = "аеоиёоуыэяю"
 print()
 for letter in message:
-    if letter.lower() not in VOMELS:
+    if letter.lower() not in VOWELS:
         new_message += letter
         print("Создана новая строка: ", new_message)
 print("\nВот ваш текст с изъятыми гласными буквами: ", new_message)
