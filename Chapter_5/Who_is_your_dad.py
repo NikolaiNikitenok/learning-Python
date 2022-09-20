427189cbe3c36cbbfc8718545458abc693a177dc
Nikolai Nikitenok <104710310+NikolaiNikitenok@users.noreply.github.com>
2022-09-20 21:06:14 +0300

    .

---
+++ Chapter_5/Who_is_your_dad.py
@@ -0,0 +1,29 @@
+# Кто твой папа?
+
+# Программа пишет отца, при написании сына
+# есть возможность добавления, изменения и удаления пар
+
+dictionary = {"Сын": "Отец",
+              "Никитенок Николай Александрович": "Никитенок Александр Сергеевич",
+              "Петров Петр Олегович": "Петров Олег Геннадиевич"}
+choice = None
+while choice != "0":
+    choice = input("Выберете: 1 - Найти, 2 - Добавить, 3 - Изменить, 4 - Удалить: ")
+    if choice == "0":
+        print("До свидания!")
+    elif choice == "1":
+        sin = input("Введите ФИО сына: ")
+        if sin in dictionary:
+            print(dictionary[sin])
+    elif choice == "2":
+        sin = input("name sin: ")
+        dad = input("name dad: ")
+        dictionary[sin] = dad
+    elif choice == "3":
+        sin = input("choose sin for edit: ")
+        dad = input("name dad: ")
+        dictionary[sin] = dad
+    elif choice == "4":
+        sin = input("choose sin for delete: ")
+        del dictionary[sin]
+
