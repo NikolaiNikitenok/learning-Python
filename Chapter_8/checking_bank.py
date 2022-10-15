5173e749a0621c06e44b72d311b414954b6d5e03
NikolaiNikitenok <k89095867511@gmail.com>
2022-10-15 15:38:52 +0300

    .

--- Chapter_8/checking_bank.py
+++ Chapter_8/checking_bank.py
@@ -1,19 +1,20 @@
-class Checking_Account(object):
+class Checking_account(object):
 
     count = 0
 
     def __init__(self, number, balance):
+        self.summa = None
         print("Add new bank balance!")
         self.number = number
         self.balance = int(balance)
-        Checking_Account.count += 1
+        Checking_account.count += 1
 
     def withdraw(self, summa):
         self.summa = summa
 
 
 def main():
-    bank1 = Checking_Account("1234 5678 9123 4567", input("Your balance: "))
+    bank1 = Checking_account("1234 5678 9123 4567", input("Your balance: "))
     print(bank1.number)
     bank1.summa = int(input("Your Summa: "))
     print(bank1.balance - bank1.summa)
---
+++ Chapter_8/critter_caretaker.py
@@ -0,0 +1,87 @@
+# Моя зверюшка
+
+# Виртуальный питомец, о котором пользователь может заботиться
+
+class Critter(object):
+    """Виртуальный питомец"""
+    def __init__(self, name, hunger=0, boredom=0):
+        self.name = name
+        self.hunger = hunger
+        self.boredom = boredom
+
+    def __pass_time(self):
+        self.hunger += 1
+        self.boredom += 1
+
+    @property
+    def mood(self):
+        unhappiness = self.hunger + self.boredom
+        if unhappiness < 5:
+            m = "Прекрасно"
+        elif 6 <= unhappiness <= 10:
+            m = "Неплохо"
+        elif 11 <= unhappiness <= 15:
+            m = "Не сказать, чтобы хорошо"
+        else:
+            m = "Ужасно"
+        return m
+
+    def talk(self):
+        print("Меня зовут", self.name, ", и сейчас я чувствую себя", self.mood, ".\n")
+        self.__pass_time()
+
+    def eat(self, food=4):
+        print("Мррр... Спасибо!")
+        self.hunger -= food
+        if self.hunger < 0:
+            self.hunger = 0
+        self.__pass_time()
+
+    def play(self, fun=4):
+        print("Уиии!")
+        self.boredom -= fun
+        if self.boredom < 0:
+            self.boredom = 0
+        self.__pass_time()
+
+
+def main():
+    crit_name = input("Как вы назовете свою зверюшку? ")
+    crit = Critter(crit_name)
+
+    choice = None
+    while choice != "0":
+        print(
+                """
+                Моя зверюшка
+                0 - Выйти
+                1 - Узнать о самочувствии зверюшки
+                2 - Покормить зверюшку
+                3 - Поиграть со зверюшкой
+                """
+            )
+        choice = input("Ваш выбор: ")
+        print()
+        # Выход
+        if choice == "0":
+            print("До свидания!")
+
+        # Беседа со зверюшкой
+        elif choice == "1":
+            crit.talk()
+
+        # Кормление зверюшки
+        elif choice == "2":
+            crit.eat()
+
+        # Игра со зверюшкой
+        elif choice == "3":
+            crit.play()
+
+        # Непонятный пользовательский ввод
+        else:
+            print("Извините, в меню нет пункта", choice)
+
+
+if __name__ == '__main__':
+    main()
