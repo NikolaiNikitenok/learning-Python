3d890b6996c43ffefe67672d5b5fde660738ead3
Nikolai Nikitenok <104710310+NikolaiNikitenok@users.noreply.github.com>
2022-11-16 21:17:53 +0300

    .

--- Chapter_9/Users.py
+++ Chapter_9/Users.py
@@ -4,6 +4,7 @@ class User():
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attempts = 0
         
     def describe_user(self):
         print(f"Это {self.first_name} {self.last_name}")
@@ -11,12 +12,25 @@ class User():
     def greet_user(self):
         print(f"Привет, дорогой {self.first_name} {self.last_name}!")
         
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+        print(f"Кол-во попыток: {self.login_attempts}!")
+        
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print(f"Кол-во попыток: {self.login_attempts}!")
+
 
 def main():
     user1 = User(input("Your name: "), input("Your Last name: "))
     user1.describe_user()
     user1.greet_user()
-
+    user1.increment_login_attempts()
+    user1.increment_login_attempts()
+    user1.increment_login_attempts()
+    user1.increment_login_attempts()
+    user1.reset_login_attempts()
+    
 
 if __name__ == "__main__":
     main()
--- Chapter_9/restaurant.py
+++ Chapter_9/restaurant.py
@@ -4,12 +4,19 @@ class Restaurant():
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
         
     def describe_restaurant(self):
         print(f"Название ресторана: {self.restaurant_name}; \nТип ресторана: {self.cuisine_type}")
         
     def open_restaurant(self):
         print(f"Ресторан {self.restaurant_name}, открыт!")
+        
+    def set_number_served(self, number):
+        self.number_served = number
+        
+    def increment_number_served(self):
+        self.number_served += 1
         
         
 def main():
@@ -19,6 +26,14 @@ def main():
     rest2.describe_restaurant()
     rest1.open_restaurant()
     rest2.open_restaurant()
+    print(rest1.number_served)
+    rest1.number_served = 300
+    print(rest1.number_served)
+    rest1.set_number_served(500)
+    print(rest1.number_served)
+    rest1.increment_number_served()
+    print(rest1.number_served)
+
     
 if __name__ == "__main__":
     main()
