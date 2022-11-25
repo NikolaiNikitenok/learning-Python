cb135542e9509b0ae12cde4f52820a7c10cad150
Nikolai Nikitenok <104710310+NikolaiNikitenok@users.noreply.github.com>
2022-11-25 12:34:52 +0300

    .

--- Chapter_9/war.py
+++ Chapter_9/war.py
@@ -1,4 +1,24 @@
 class Card():
     """ Создает колоду карт, каждая карта имеет свой номинал. """
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    SUITS = ["Трефы", "Бубны", "Червы", "Пики"]
+    card = ''
+    coloda = []
+    
     def __init__(self):
-        self
+        pass
+    
+    def coloda_create(self, rank, suit):
+        self.card = str(rank) + '-' + str(suit)
+        self.coloda.append(self.card)
+        
+    
+    def card_create(self):
+        for rank in self.RANKS:
+            for suit in self.SUITS:
+                self.coloda_create(rank, suit)
+                
+                
+        
+Card().card_create()
+print(Card().coloda)
