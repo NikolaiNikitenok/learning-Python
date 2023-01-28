d3d634f758c8b36a974bf8319d566837a81b88d0
Nikolai Nikitenok <104710310+NikolaiNikitenok@users.noreply.github.com>
2023-01-28 19:12:12 +0300

    new command

--- bots/example.py
+++ bots/example.py
@@ -7,6 +7,12 @@ async def start_handler(event: types.Message):
         f"Hello, {event.from_user.get_mention(as_html=True)}?!", parse_mode=types.ParseMode.HTML,
     )
     
+
+async def how_handler(event: types.Message):
+    await event.answer(
+        f"Okay, i'm ready!"
+    )
+    
     
 async def main():
     bot = Bot(token="5987927045:AAFpCElru0cA794E3NJqwi8Zx0KRYgXMxgM")
@@ -14,6 +20,9 @@ async def main():
     try:
         disp = Dispatcher(bot=bot)
         disp.register_message_handler(start_handler, commands={"start", "restart"})
+        
+        disp.register_message_handler(how_handler, commands="go")
+        
         await disp.start_polling()
     finally:
         await bot.close()
