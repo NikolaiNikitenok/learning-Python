9b2c1cc62c8bdc50c064c15bc48956b417558849
Nikolai Nikitenok <104710310+NikolaiNikitenok@users.noreply.github.com>
2023-01-29 08:22:17 +0300

    new command: send sticker

--- bots/simple_bot.py
+++ bots/simple_bot.py
@@ -1,43 +1,60 @@
-import logging
+# import logging
 from aiogram import Bot, Dispatcher, executor, types
-from config import API_TOKEN, HELP_COMMANDS, ALPHABET, DESCRIPTION
+# from config import API_TOKEN, HELP_COMMANDS, ALPHABET, DESCRIPTION
+import config
 import random
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=config.API_TOKEN)
 dp = Dispatcher(bot)
 
 count = 1
 
+
+async def on_startup(_):
+    print("Bot is ready! Let's fxcking go!")
+
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message=types.Message):
-    await message.answer(f"Hi, I'm Echo BOT!\nPowered by aiogram.")
-    await message.answer(HELP_COMMANDS)
+    await message.answer(f"Hi, I'm <em>Echo BOT</em>!\n<b>Powered by aiogram.</b>", parse_mode="HTML")
+    await message.answer(config.HELP_COMMANDS, parse_mode="HTML")
     await message.delete()
-    
+
+
 @dp.message_handler(commands=['add'])
 async def add_smth(message: types.Message):
     await message.answer(f"Что добавить?")
     await message.delete()
-    
+
+
 @dp.message_handler(commands=['random'])
 async def random_letter(message: types.Message):
-    await message.answer(f"Random letter: {random.choice(ALPHABET)}")
+    await message.answer(f"Random letter: {random.choice(config.ALPHABET)}")
     await message.delete()
-    
+
+
 @dp.message_handler(commands=['description'])
 async def description(message: types.Message):
-    await message.answer(DESCRIPTION)
+    await message.answer(config.DESCRIPTION)
     await message.delete()
-    
+
+
 @dp.message_handler(commands=['count'])
 async def counter(message: types.Message):
     global count
     await message.answer(f'Now count = {count}!')
     await message.delete()
     count += 1
-    
+
+
+@dp.message_handler(commands=["give"])
+async def give_sticker(message: types.Message):
+    await bot.send_sticker(message.from_user.id, sticker=config.STICKER_ID)
+    await message.delete()
+
+
 @dp.message_handler()
 async def echo_upper(message: types.Message):
     
@@ -52,4 +69,4 @@ async def echo_upper(message: types.Message):
     
     
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)   
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
